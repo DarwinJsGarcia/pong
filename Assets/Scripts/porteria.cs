d1095766aefab3a4483fc69ae6aec152fa8ef488
unknown <darwingarcia930@gmail.com>
2020-10-27 17:43:11 -1100

    primer commit proyecto creado

---
+++ Assets/Scripts/porteria.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class porteria : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D bola){
+
+        if(bola.name == "Bola"){
+            if(this.name == "izquierda"){
+              
+              bola.GetComponent<Bola>().reiniciarBola("derecha");
+            }else if(this.name == "derecha"){
+                
+              bola.GetComponent<Bola>().reiniciarBola("izquierda");
+            }
+        }
+    }
+}
